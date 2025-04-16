8cd2a89838b370b0a85802774080509baac4e6ce
YongHC <yonghc-sm24@student.tarc.edu.my>
2025-04-16 14:21:42 +0800

    Add files via upload

--- Programme.java
+++ Programme.java
@@ -1,18 +1,20 @@
 // Yong Hao Cheng 2401385
 // DSF1S3
 
+import java.util.Arrays;
+
 public class Programme {
 	private String programmeID;
 	private String programmeLeader;
 	private int programmeDuration;
-	private Course courseList;
+	private Course[] courseList = new Course[15];
 	
 	//constructors
 	public Programme(){
-		this(" "," ",0,new Course());
+		this(" "," ",0,new Course[15]);
 	}
 	public Programme(String programmeID, String programmeLeader,
-	int programmeDuration, Course courseList){
+	int programmeDuration, Course[] courseList){
 		this.programmeID = programmeID;
 		this.programmeLeader = programmeLeader;
 		this.programmeDuration = programmeDuration;
@@ -29,29 +31,39 @@ public class Programme {
 	public void setProgrammeDuration(int programmeDuration){
 		this.programmeDuration = programmeDuration;
 	}
-	public void setCourseList(Course courseList){
+	public void setCourseList(Course[] courseList){
 		this.courseList = courseList;
 	}
 	
 	//getter
-	public String ProgrammeID(){
+	public String getProgrammeID(){
 		return programmeID;
 	}
-	public String ProgrammeLeader(){
+	public String getProgrammeLeader(){
 		return programmeLeader;
 	}
-	public int ProgrammeDuration(){
+	public int getProgrammeDuration(){
 		return programmeDuration;
 	}
-	public Course CourseList(){
+	public Course[] getCourseList(){
 		return courseList;
+	}
+	
+	//addCourse
+	public void addCourse(Course course){
+		for(int i = 0; i < courseList.length; i++){
+			if(courseList[i] == null){
+				this.courseList[i] = course;
+				break;
+			}
+		}
 	}
 	
 	//toString
 	public String toString(){
-		return "ProgrammeID: " + programmeID
+		return "ProgrammeID: " + programmeID 
 				+ "\nProgramme Leader: " + programmeLeader
 				+ "\nProgramme Duration: " + programmeDuration
-				+ "\nCourse List: " + courseList;
+				+ "\nCourse List: " + Arrays.toString(courseList);
 	}
 }
