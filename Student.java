a389573c933312be033bf0000f0062b62641df98
bitpng <rowanyxp-sm24@student.tarc.edu.my>
2025-04-30 19:21:02 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,79 +1,116 @@
 // Rowan Yee Xiao Peng
 // DSF Y1S3
 
-public class Student {
-	private String studentID;
+import java.util.Arrays;
+
+public class Student extends Person {
+    private String studentID;
     private int yearOfStudy;
     private Course[] courseList;
     private Programme programme;
     private Result result;
     private Exam[] examList;
-    
+
     // constructor
-    public Student(String studentID, int yearOfStudy, Course[] courseList, Programme programme, Result result, Exam[] examList) {
-    	this.studentID = studentID;
+    public Student(String name, String email, String contactNo, String studentID, int yearOfStudy, Course[] courseList, Programme programme, Result result, Exam[] examList) {
+        super(name, email, contactNo);
+        this.studentID = studentID;
         this.yearOfStudy = yearOfStudy;
-        this.courseList = courseList;
+        this.courseList = courseList != null ? Arrays.copyOf(courseList, courseList.length) : new Course[0];
         this.programme = programme;
         this.result = result;
-        this.examList = examList;
+        this.examList = examList != null ? Arrays.copyOf(examList, examList.length) : new Exam[0];
     }
-    
-    // setter
+
+    // setters
     public void setStudentID(String studentID) {
-    	this.studentID = studentID;
-    	}
-	public void setYearOfStudy(int yearOfStudy) {
-		this.yearOfStudy = yearOfStudy;
-		}
-	public void setCourseList(Course[] courseList) {
-		this.courseList = courseList;
-		}
-	public void setProgramme(Programme programme) {
-		this.programme = programme;
-		}
-	public void setResult(Result result) {
-		this.result = result;
-		}
-	public void setExamList(Exam[] examList) {
-		this.examList = examList;
-		}
-    
-    // getter
+        this.studentID = studentID;
+    }
+
+    public void setYearOfStudy(int yearOfStudy) {
+        this.yearOfStudy = yearOfStudy;
+    }
+
+    public void setCourseList(Course[] courseList) {
+        this.courseList = Arrays.copyOf(courseList, courseList.length);
+    }
+
+    public void setProgramme(Programme programme) {
+        this.programme = programme;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public void setExamList(Exam[] examList) {
+        this.examList = Arrays.copyOf(examList, examList.length);
+    }
+
+    // getters
     public String getStudentID() {
-    	return studentID; 
-    	}
+        return studentID;
+    }
+
     public int getYearOfStudy() {
-    	return yearOfStudy;
-    	}
+        return yearOfStudy;
+    }
+
     public Course[] getCourseList() {
-    	return courseList;
-    	}
+        return Arrays.copyOf(courseList, courseList.length);
+    }
+
     public Programme getProgramme() {
-    	return programme;
-    	}
+        return programme;
+    }
+
     public Result getResult() {
-    	return result;
-    	}
+        return result;
+    }
+
     public Exam[] getExamList() {
-    	return examList;
-    	}
+        return Arrays.copyOf(examList, examList.length);
+    }
+
+    // register course
+    public void registerCourse(Course course) {
+        if (course == null) return;
+
+        for (Course c : courseList) {
+            if (c != null && c.getCourseID().equals(course.getCourseID())) {
+                return;
+            }
+        }
+
+        Course[] newList = Arrays.copyOf(courseList, courseList.length + 1);
+        newList[newList.length - 1] = course;
+        courseList = newList;
+    }
     
-    public void registerCourse(Course courseID) {
-    }
+    // unregister course
+    public void unregisterCourse(Course course) {
+        if (course == null) return;
 
-    public void unregisterCourse(Course courseID) {
+        courseList = Arrays.stream(courseList)
+                           .filter(c -> c != null && !c.getCourseID().equals(course.getCourseID()))
+                           .toArray(Course[]::new);
     }
-
-    @Override
+    
+    // tostring
     public String toString() {
-        return "StudentID: " + studentID + ", Year: " + yearOfStudy + ", Programme: " + programme.getProgrammeID();
+        return super.toString() +
+               "\nStudentID: " + studentID +
+               "\nYear: " + yearOfStudy +
+               "\nProgramme: " + (programme != null ? programme.getProgrammeID() : "None") +
+               "\nCourses: " + Arrays.toString(courseList) +
+               "\nResult: " + (result != null ? result.toString() : "No result");
     }
-
+    
+    // equals
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (!(obj instanceof Student)) return false;
         Student other = (Student) obj;
-        return this.studentID.equals(other.studentID);
+        return this.studentID != null && this.studentID.equals(other.studentID);
     }
 }
